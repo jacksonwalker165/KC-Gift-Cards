995672978d58f1291a109165cf6ee481e3343185
mudi-igbinoba <ruth.igbinoba12@gmail.com>
2022-04-04 14:16:32 +0100

    made corrections to currency section

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,13 +9,15 @@ const Cards = ({item}) => {
         <Card className='h-100 bg-secondary bg-gradient shadow'>
             <Card.Header className='fw-bolder text-dark'>Gift Card {item.id}</Card.Header>
             <Card.Body>
-                <Card.Text className="text-white"><strong className='text-dark'>Name:</strong> <br/> {item.title}</Card.Text>
-                <Card.Text className="text-white"><strong className='text-dark'>Currencies:</strong>
-                    <br/>
-                    {
-                    item.currencies.map(e => (`"${e}"` + ',' + ' '))
-                    }
-                </Card.Text>
+                <Card.Text className="text-white"><strong className='text-dark'>Name:</strong> <br/>{item.title}</Card.Text>
+                <Card.Text className="text-white"><strong className='text-dark'>Currencies:</strong></Card.Text>
+                <div>
+
+                  {item.currencies.map(e => (
+                    <p className='bg-light bg-gradient rounded py-1'>{e}</p>
+                  ))}
+              
+                </div>
             </Card.Body>
         </Card>
     </Col>
